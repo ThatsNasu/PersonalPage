df70dbdd42cc8d3ffd12dc006b090efbb51b0484
netcodedev <benmondini@netcode.dev>
2024-02-02 16:57:43 +0100

    Fixed post view

--- src/routes/devlog/[topic]/[post]/+page.ts
+++ src/routes/devlog/[topic]/[post]/+page.ts
@@ -1,12 +1,23 @@
+import type { Post } from '$lib/types';
 import { error } from '@sveltejs/kit'
 
 export async function load({ params }) {
 	try {
-		const post = await import(`/src/posts/${params.topic}/${params.post}.md`)
-        
-		return {
-			content: post.default,
-			meta: post.metadata
+		const paths = await import.meta.glob(`/src/posts/openirc/1.md`, { eager: true });
+
+		for (const path in paths) {
+			const file = paths[path];
+			const topic = path.split('/').at(-2);
+			if(topic !== params.topic) continue;
+			const slug = topic+"/"+path.split('/').at(-1)?.replace('.md', '');
+	
+			if (file && typeof file === 'object' && 'metadata' in file && 'default' in file && slug) {
+				const metadata = file.metadata as Omit<Post, 'slug'>;
+				return {
+					meta: metadata,
+					content: file.default
+				}
+			}
 		}
 	} catch (e) {
 		throw error(404, `Could not find post ${params.post}`)
