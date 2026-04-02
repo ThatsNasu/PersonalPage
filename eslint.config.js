4b91726f9e0a7f491c63a49ccfc57d6d616eab91
Hauke Motzkus <motzkus.hauke@gmail.com>
2026-04-02 15:45:06 +0200

    running prettier again

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,34 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import svelte from "eslint-plugin-svelte";
-import prettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
 
 export default [
 	// Ignore build + generated files
 	{
-	ignores: [
-		"node_modules",
-		".svelte-kit",
-		"build",
-		"dist",
-		"package",
-		"*.md"
-	]
+		ignores: ['node_modules', '.svelte-kit', 'build', 'dist', 'package', '*.md']
 	},
 	js.configs.recommended,
 	...tseslint.configs.recommended,
-	...svelte.configs["flat/recommended"],
+	...svelte.configs['flat/recommended'],
 	{
-	files: ["**/*.ts", "**/*.js", "**/*.svelte"],
-	languageOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module"
-	},
-	rules: {
-		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": [
-		"warn",
-		{
-			argsIgnorePattern: "^_"
+		files: ['**/*.ts', '**/*.js', '**/*.svelte'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module'
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_'
+				}
+			],
+			'no-console': 'off',
+			'prefer-const': 'warn',
+			'no-var': 'error'
 		}
-		],
-		"no-console": "off",
-		"prefer-const": "warn",
-		"no-var": "error"
-	}
 	},
 	prettier
 ];
